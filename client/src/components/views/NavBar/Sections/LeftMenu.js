99200e18d1f64500def5da8d2d926a4822a4d8c5
Narendra Jani <89453812+Narendra-027@users.noreply.github.com>
2023-05-02 10:49:31 +0530

    order view updated

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -1,27 +1,51 @@
 import React from 'react';
 import { Menu } from 'antd';
+import axios from 'axios';
+import { USER_SERVER } from '../../../Config';
+import { withRouter } from 'react-router-dom';
+import { useSelector } from "react-redux";
+
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 function LeftMenu(props) {
-  return (
-    <Menu mode={props.mode}>
-    <Menu.Item key="mail">
-      <a href="/">Home</a>
-    </Menu.Item>
-    <SubMenu title={<span>About Us</span>}>
-      <MenuItemGroup title="Who are we ?">
-        <Menu.Item key="setting:1"><a href="/team">Team</a></Menu.Item>
-        <Menu.Item key="setting:2"><a href="/service">Our Services</a></Menu.Item>
-        <Menu.Item key="setting:3"><a href="/reviews">Reviews</a></Menu.Item>
-        <Menu.Item key="setting:4"><a href="/contacts">Contact Us</a></Menu.Item>
-      </MenuItemGroup>
-    </SubMenu>
-    <Menu.Item key="setting:5">
-      <a href="/orderlist">Orders</a>
-    </Menu.Item>
-  </Menu>
-  )
+  const user = useSelector(state => state.user)
+  if (user.userData && !user.userData.isAuth){
+    return (
+      <Menu mode={props.mode}>
+      <Menu.Item key="mail">
+        <a href="/">Home</a>
+      </Menu.Item>
+      <SubMenu title={<span>About Us</span>}>
+        <MenuItemGroup title="Who are we ?">
+          <Menu.Item key="setting:1"><a href="/team">Team</a></Menu.Item>
+          <Menu.Item key="setting:2"><a href="/service">Our Services</a></Menu.Item>
+          <Menu.Item key="setting:3"><a href="/reviews">Reviews</a></Menu.Item>
+          <Menu.Item key="setting:4"><a href="/contacts">Contact Us</a></Menu.Item>
+        </MenuItemGroup>
+      </SubMenu>
+    </Menu>
+    )
+  }else{
+    return (
+      <Menu mode={props.mode}>
+      <Menu.Item key="mail">
+        <a href="/">Home</a>
+      </Menu.Item>
+      <SubMenu title={<span>About Us</span>}>
+        <MenuItemGroup title="Who are we ?">
+          <Menu.Item key="setting:1"><a href="/team">Team</a></Menu.Item>
+          <Menu.Item key="setting:2"><a href="/service">Our Services</a></Menu.Item>
+          <Menu.Item key="setting:3"><a href="/reviews">Reviews</a></Menu.Item>
+          <Menu.Item key="setting:4"><a href="/contacts">Contact Us</a></Menu.Item>
+        </MenuItemGroup>
+      </SubMenu>
+      <Menu.Item key="setting:5">
+        <a href="/orderlist">Orders</a>
+      </Menu.Item>
+    </Menu>
+    )
+  }
 }
 
 export default LeftMenu
